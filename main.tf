b51685fe380ee22a10f38f5b72463554f8ebeef3
peeyushk20 <86283047+peeyushk20@users.noreply.github.com>
2021-08-25 11:33:13 +0530

    added variables

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "eastus"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "random_id" "storage_account" {
@@ -24,9 +24,9 @@ resource "random_id" "storage_account" {
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = "mytfsa${lower(random_id.storage_account.hex)}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  name                     = "${var.storage_account_name}${lower(random_id.storage_account.hex)}"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "resource_group_name" {
+ description = "Azure resourse group name"
+ default="myTfGroup1"
+}
+
+variable "location"{
+ default = "eastus"
+}
+
+variable "storage_account_name" {
+ description = "Azure storage account name"
+ default="mytfsa"
+}
+
+
+
